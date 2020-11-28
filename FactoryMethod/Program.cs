57106b86cd90b53fcd6d138759a0c83d3ab3b406
Jscroop <57612131+Jscroop@users.noreply.github.com>
2020-11-28 21:12:26 +0800

    FactoryMethod

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFactory factoryA = new FactoryA();
+            var productA = factoryA.CreateProduct();
+            productA.ShowName();
+
+            IFactory factoryB = new FactoryB();
+            var productB = factoryB.CreateProduct();
+            productB.ShowName();
+
+            Console.ReadKey();
+        }
+    }
+
+    //抽象产品
+    public interface IProduct
+    {
+        void ShowName();
+    }
+
+    //具体产品
+    public class ProductA : IProduct
+    {
+        public void ShowName()
+        {
+            Console.WriteLine("我是产品A");
+        }
+    }
+
+    public class ProductB : IProduct
+    {
+        public void ShowName()
+        {
+            Console.WriteLine("我是产品B");
+        }
+    }
+
+
+    //抽象工厂
+    public interface IFactory
+    {
+        IProduct CreateProduct();
+    }
+
+    //具体工厂
+    public class FactoryA : IFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new ProductA();
+        }
+    }
+
+    public class FactoryB : IFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new ProductB();
+        }
+    }
+
+
+}
--- SimpleFactory/Program.cs
+++ SimpleFactory/Program.cs
@@ -6,35 +6,93 @@ namespace SimpleFactory
     {
         static void Main(string[] args)
         {
-            SimpleFactory.Create(Product.ProductA);
-            SimpleFactory.Create(Product.ProductB);
-            SimpleFactory.Create(Product.ProductC);
+            var productA = SimpleFactory.GetProduct(ProductType.ProductA);
+            productA?.Product();
+
+            var productB = SimpleFactory.GetProduct(ProductType.ProductB);
+            productB?.Product();
+
+            var productC = SimpleFactory.GetProduct(ProductType.ProductC);
+            productC?.Product();
+
             Console.ReadKey();
         }
     }
 
+    //抽象产品
+    public interface IProduct
+    {
+        void Product();
+    }
+
+    //具体产品
+    public class ProductA : IProduct
+    {
+        public ProductA()
+        {
+            Console.WriteLine("开始生产产品A！");
+        }
+
+        public void Product()
+        {
+            Console.WriteLine("产品A生产成功！");
+        }
+    }
+
+    //具体产品
+    public class ProductB : IProduct
+    {
+        public ProductB()
+        {
+            Console.WriteLine("开始生产产品B！");
+        }
+        public void Product()
+        {
+            Console.WriteLine("产品B生产成功!");
+        }
+    }
+
+    //具体产品
+    public class ProductC : IProduct
+    {
+        public ProductC()
+        {
+            Console.WriteLine("开始生产产品C！");
+        }
+
+        public void Product()
+        {
+            Console.WriteLine("产品C生产成功!");
+        }
+    }
+
+    //生产工厂
     public static class SimpleFactory
     {
-        public static void Create(Product product)
+        public static IProduct GetProduct(ProductType productType)
         {
-            switch (product)
+            IProduct product = null;
+
+            switch (productType)
             {
-                case Product.ProductA:
-                    Console.WriteLine("产品A生产成功！");
+                case ProductType.ProductA:
+                    product = new ProductA();
                     break;
-                case Product.ProductB:
-                    Console.WriteLine("产品B生产成功!");
+                case ProductType.ProductB:
+                    product = new ProductB();
                     break;
-                case Product.ProductC:
-                    Console.WriteLine("产品C生产成功!");
+                case ProductType.ProductC:
+                    product = new ProductC();
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(product), product, "产品不存在，请确认!");
             }
+
+            return product;
         }
     }
 
-    public enum Product
+    public enum ProductType
     {
         ProductA,
         ProductB,
