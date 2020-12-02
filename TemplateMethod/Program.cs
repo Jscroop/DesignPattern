a2e19c2c25e4fea01e88db935f19a8c5f2ec9f44
Jscroop <57612131+Jscroop@users.noreply.github.com>
2020-12-02 19:52:13 +0800

    TemplateMethod

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hpComputer = new AssembleHpComputer();
+            var dellComputer = new AssembleDellComputer();
+
+            hpComputer.Assemble();
+            dellComputer.Assemble();
+
+            Console.ReadKey();
+        }
+    }
+
+    //抽象类
+    public abstract class AssembleComputer
+    {
+        public abstract void BuildMainFramePart();
+
+        public abstract void BuildScreenPart();
+
+        public abstract void BuildInputPart();
+        public void Assemble()
+        {
+            BuildMainFramePart();
+            BuildScreenPart();
+            BuildInputPart();
+        }
+    }
+
+    //具体子类1
+    public class AssembleHpComputer : AssembleComputer
+    {
+        public override void BuildMainFramePart()
+        {
+            Console.WriteLine("组装HP电脑的主板");
+        }
+
+        public override void BuildScreenPart()
+        {
+            Console.WriteLine("组装HP电脑的显示器");
+        }
+
+        public override void BuildInputPart()
+        {
+            Console.WriteLine("组装HP电脑的键盘鼠标");
+        }
+    }
+
+    //具体子类2
+    public class AssembleDellComputer : AssembleComputer
+    {
+        public override void BuildMainFramePart()
+        {
+            Console.WriteLine("组装Dell电脑的主板");
+        }
+
+        public override void BuildScreenPart()
+        {
+            Console.WriteLine("组装Dell电脑的显示器");
+        }
+
+        public override void BuildInputPart()
+        {
+            Console.WriteLine("组装Dell电脑的键盘鼠标");
+        }
+    }
+}
